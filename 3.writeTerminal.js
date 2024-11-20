b03ef8a24f2556d2439f0678a0d96caddd9881c5
Harry <harryrizky139@gmail.com>
2024-11-20 09:17:54 +0700

    menambahkan comments di 3.writeTerminal.js

--- 3.writeTerminal.js
+++ 3.writeTerminal.js
@@ -1,9 +1,14 @@
+// Memanggil Modul Readline
 const readline = require('node:readline');
+// Memanggil Modul Prosess
 const { stdin: input, stdout: output } = require('node:process');
 
+// membuat constanta rl yang memiliki nilai fuction createInterface untuk input dan output di terminal
 const rl = readline.createInterface({ input, output });
 
+// menggunakan method question dari module readline yang nilai namanya akan di masukan di callback answer
 rl.question('Nama : ', (answer) => {
+  // menampilkan string dengan console log dan jawaban dari callback answer
   console.log(`Nama Mu Adalah : ${answer}`);
     rl.question('Nomor Ponsel : ', (answer) => {
       console.log(`Nomor Ponsel Mu Adalah : ${answer}`);
@@ -11,6 +16,7 @@ rl.question('Nama : ', (answer) => {
         console.log(`Email Mu Adalah: ${answer}`);
 
         console.log('Terimakasih Sudah Mengisi!!!');
+      // menutup interface
       rl.close();
     });
   });
